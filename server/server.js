0557778566412c03f0c0ef5f4d23952c41c0a209
oneilljp-keyin <john.oneill@keyin.com>
2021-08-16 18:04:44 -0230

    finished server side register and login

--- server/server.js
+++ server/server.js
@@ -3,25 +3,38 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const database = require("./api/database.routes.js");
-const passport = require("passport");
-require("./passport/setup");
-const auth = require("./api/auth");
+const users = require("./api/users");
 
 const app = express();
 
 MONGO_URI = process.env.MONGO_DB_CONNECTION_STRING;
-// console.log(MONGO_URI);
 
-// mongoose
-//   .connect(MONGO_URI, { useNewUrlParser: true })
-//   .then(console.log(`MongoDB connection for Authorization`))
-//   .catch((err) => console.log(err.message));
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(console.log(`MongoDB connection for Authorization`))
+  .catch((err) => console.log(err.message));
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+
+// Body-Parser middleware
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+// app.use(bodyParser.json());
+
+// Passport config
+require("./validation/passport")(passport);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express Session
 const secretKey = process.env.SESSION_SECRET || "qwertyuiop1234567890";
@@ -34,13 +47,9 @@ app.use(
   })
 );
 
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
 // Routes
 app.use("/api/v1/sfdatabase", database);
-app.use("/api/auth", auth);
+app.use("/api/users", users);
 app.use("*", (req, res) => res.status(404).json({ error: "Page Not Found" }));
 
 module.exports = app;
